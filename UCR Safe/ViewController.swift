3b4fffaf632bd4e84d5abf63c1577741f02702a3
shrutix <shrspam8@gmail.com>
2021-04-11 00:04:15 -0700

    full workspace

---
+++ UCR Safe/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  UCR Safe
+//
+//  Created by Shruti Jana on 4/9/21.
+//  Copyright © 2021 Shruti J. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func escortCall(_ sender: UIButton) {
+        guard let numberString = sender.titleLabel?.text, let url = URL(string: "teleprompt://9518273772") else {
+            return
+        } /*\(numberString)*/
+        
+        UIApplication.shared.open(url)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
